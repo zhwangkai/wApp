b1e01794adc53132676e6814e029f9d30d0b1207
zhwangkai <zhangweicoding@outlook.com>
2020-05-13 16:14:08 +0800

    init wApp

---
+++ build/index.bundle.js
@@ -0,0 +1,100 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/_babel-loader@8.1.0@babel-loader/lib/index.js):\\nError: Cannot find module '@babel/plugin-transform-modules-amd'\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:636:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:562:25)\\n    at Module.require (internal/modules/cjs/loader.js:692:17)\\n    at require (/usr/local/workspace/wApp/node_modules/_v8-compile-cache@2.0.3@v8-compile-cache/v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (/usr/local/workspace/wApp/node_modules/_@babel_preset-env@7.9.6@@babel/preset-env/lib/available-plugins.js:72:57)\\n    at Module._compile (/usr/local/workspace/wApp/node_modules/_v8-compile-cache@2.0.3@v8-compile-cache/v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:789:10)\\n    at Module.load (internal/modules/cjs/loader.js:653:32)\\n    at tryModuleLoad (internal/modules/cjs/loader.js:593:12)\\n    at Function.Module._load (internal/modules/cjs/loader.js:585:3)\\n    at Module.require (internal/modules/cjs/loader.js:692:17)\\n    at require (/usr/local/workspace/wApp/node_modules/_v8-compile-cache@2.0.3@v8-compile-cache/v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (/usr/local/workspace/wApp/node_modules/_@babel_preset-env@7.9.6@@babel/preset-env/lib/plugins-compat-data.js:12:48)\\n    at Module._compile (/usr/local/workspace/wApp/node_modules/_v8-compile-cache@2.0.3@v8-compile-cache/v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:789:10)\\n    at Module.load (internal/modules/cjs/loader.js:653:32)\\n    at tryModuleLoad (internal/modules/cjs/loader.js:593:12)\\n    at Function.Module._load (internal/modules/cjs/loader.js:585:3)\\n    at Module.require (internal/modules/cjs/loader.js:692:17)\\n    at require (/usr/local/workspace/wApp/node_modules/_v8-compile-cache@2.0.3@v8-compile-cache/v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (/usr/local/workspace/wApp/node_modules/_@babel_preset-env@7.9.6@@babel/preset-env/lib/normalize-options.js:20:26)\\n    at Module._compile (/usr/local/workspace/wApp/node_modules/_v8-compile-cache@2.0.3@v8-compile-cache/v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:789:10)\\n    at Module.load (internal/modules/cjs/loader.js:653:32)\\n    at tryModuleLoad (internal/modules/cjs/loader.js:593:12)\\n    at Function.Module._load (internal/modules/cjs/loader.js:585:3)\\n    at Module.require (internal/modules/cjs/loader.js:692:17)\\n    at require (/usr/local/workspace/wApp/node_modules/_v8-compile-cache@2.0.3@v8-compile-cache/v8-compile-cache.js:161:20)\\n    at Object.<anonymous> (/usr/local/workspace/wApp/node_modules/_@babel_preset-env@7.9.6@@babel/preset-env/lib/index.js:19:48)\\n    at Module._compile (/usr/local/workspace/wApp/node_modules/_v8-compile-cache@2.0.3@v8-compile-cache/v8-compile-cache.js:192:30)\");\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+/******/ });
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: path.join(__dirname, 'src', 'index.js'),
+	output: { path: path.join(__dirname, 'build'), filename: 'index.bundle.js' },
+	mode: process.env.NODE_ENV || 'development',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+					}
+		  },
+		  {
+			test: /\.html$/,
+			use: [
+			{
+				loader: "html-loader"
+				}
+				]
+				}
+				]
+				},
+				
+				resolve: {modules: [path.resolve(__dirname, 'src'), 'node_modules']},
+				
+				plugins: [
+				new HtmlWebPackPlugin({
+				template: path.join(__dirname, 'public','index.html')
+				})
+				]
+				};
