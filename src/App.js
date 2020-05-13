ebaaba7bec2c1efc262b317a1209f7da981d719e
Wei Zhang <wsm@192.168.0.111>
2020-05-13 23:48:46 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Swirl from "./templates/Swirl"
 
 class App extends Component {
     render() {
         return (
-            <div>Nice</div>
+            <Swirl />
         )
     }
 }
