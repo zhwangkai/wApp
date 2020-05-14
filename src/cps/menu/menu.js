cde06be3a0d37797210cd7cbb2a8b1152496144f
Wei Zhang <wsm@192.168.0.103>
2020-05-14 23:01:36 +0800

    update button

--- src/cps/menu/menu.js
+++ src/cps/menu/menu.js
@@ -6,9 +6,7 @@ class Menu extends Component {
 	render() {
 		return (
 		<div className='cp cp-menu-container'>
-			<svg t="1589467129714" className="btn btn-menu icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4379" width="200" height="200"><path d="M128 256h64V192H128zM320 256h576V192H320zM128 544h64v-64H128zM320 544h576v-64H320zM128 832h64v-64H128zM320 832h576v-64H320z" fill="#ffffff" p-id="4380"></path></svg>
-
-
+			<svg t="1589467129714" className="btn btn-menu icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4379" width="35" height="35"><path d="M128 256h64V192H128zM320 256h576V192H320zM128 544h64v-64H128zM320 544h576v-64H320zM128 832h64v-64H128zM320 832h576v-64H320z" fill="#ffffff" p-id="4380"></path></svg>
 		</div>
 		)
 	}
