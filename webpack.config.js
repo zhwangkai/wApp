f5e9052e95896fe3d4e092ab11a8aac13a14c7b7
kaism <62653445+zhwangkai@users.noreply.github.com>
2021-04-27 23:18:32 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,6 @@ module.exports = {
                     }
                 ]
             },  
-                
             // {
             //     test: /\.svg$/,
             //     loader: 'svg-sprite-loader',
